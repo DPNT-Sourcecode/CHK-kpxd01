cd704f676f49b40f6ba7abb55bc3ae8fa8019acb
sbx_user1051 <sbx_user1051@169.254.5.210>
2024-12-03 13:27:39 +0000

    Tue Dec 03 13:27:39 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -26,4 +26,22 @@ def test_multiple_items_no_special_offers():
     assert cs.checkout("ABCD") == 115
 
 
+def test_special_offers():
+    assert cs.checkout("AAA") == 130
+    assert cs.checkout("BB") == 45
+
+    assert cs.checkout("AAAA") == 180
+    assert cs.checkout("BBB") == 75
+
+
+def test_multiple_items_special_offers():
+    assert cs.checkout("AAAC") == 150
+    assert cs.checkout("AAAAD") == 195
+    assert cs.checkout("AAABB") == 175
+    assert cs.checkout("AAAABBB") == 255
+    assert cs.checkout("AAAAAA") == 260
+    assert cs.checkout("AAAAAAA") == 310
+
+
+
 
