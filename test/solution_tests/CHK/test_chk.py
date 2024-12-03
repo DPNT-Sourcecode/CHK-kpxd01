49c1fd3fdbe1688fa3ee3189525184f9757880ba
sbx_user1051 <sbx_user1051@169.254.5.210>
2024-12-03 13:18:39 +0000

    Tue Dec 03 13:18:39 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,8 +1,19 @@
-from solutions.CHK import checkout_solution
+from solutions.CHK import checkout_solution as cs
 
 
 def test_single_items():
-    assert checkout_solution.checkout("A") == 50
-    assert checkout_solution.checkout("B") == 30
-    assert checkout_solution.checkout("C") == 20
-    assert checkout_solution.checkout("D") == 15
+    assert cs.checkout("A") == 50
+    assert cs.checkout("B") == 30
+    assert cs.checkout("C") == 20
+    assert cs.checkout("D") == 15
+
+
+def test_invalid_input():
+    assert cs.checkout(None) == -1
+    assert cs.checkout("") == -1
+    assert cs.checkout("E") == -1
+    assert cs.checkout(0) == -1
+    assert cs.checkout("a") == -1
+    assert cs.checkout("Aa") == -1
+    assert cs.checkout("aA") == -1
+
