c687208476949fd166bafbb310a4b809456bf36b
sbx_user1051 <sbx_user1051@169.254.5.210>
2024-12-03 13:09:39 +0000

    Tue Dec 03 13:09:39 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,26 @@
+PRICES = {"A": 50, "B": 30, "C": 20, "D": 15}
+
+SPECIAL_OFFERS = {"A": (3, 130), "B": (2, 45)}
 
 
-# noinspection PyUnusedLocal
-# skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    item_counts = {}
+    for sku in skus:
+        if sku not in PRICES:
+            return -1
+        item_counts[sku] = item_counts[sku].get(sku, 0) + 1
+
+    total_price = 0
+
+    for item, count in item_counts.items():
+        if item in SPECIAL_OFFERS:
+            offer_quantity, offer_price = SPECIAL_OFFERS[item]
+            n_offers = count // offer_quantity
+            total_price += offer_price * n_offers
+            remaining = count % offer_quantity
+            total_price += remaining * PRICES[item]
+        else:
+            total_price += count * PRICES[item]
+
+    return total_price
+
