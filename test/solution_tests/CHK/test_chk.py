1899dfbdcdb789ff8cc2018eace63582c553b8f6
sbx_user1051 <sbx_user1051@169.254.5.210>
2024-12-03 14:10:12 +0000

    Tue Dec 03 14:10:12 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -6,11 +6,11 @@ def test_single_items():
     assert cs.checkout("B") == 30
     assert cs.checkout("C") == 20
     assert cs.checkout("D") == 15
+    assert cs.checkout("E") == 40
 
 
 def test_invalid_input():
     assert cs.checkout(None) == -1
-    assert cs.checkout("E") == -1
     assert cs.checkout(0) == -1
     assert cs.checkout("a") == -1
     assert cs.checkout("0") == -1
@@ -27,11 +27,16 @@ def test_multiple_items_no_special_offers():
     assert cs.checkout("ABCD") == 115
 
 
-def test_special_offers():
+def test_special_offer_discount():
     assert cs.checkout("AAA") == 130
+    assert cs.checkout("AAAAA") == 200
     assert cs.checkout("BB") == 45
 
     assert cs.checkout("AAAA") == 180
+    assert cs.checkout("AAAAAA") == 250
+    assert cs.checkout("AAAAAAAA") == 330
+    assert cs.checkout("AAAAAAAAA") == 380
+    assert cs.checkout("AAAAAAAAAA") == 400
     assert cs.checkout("BBB") == 75
 
 
@@ -40,5 +45,4 @@ def test_multiple_items_special_offers():
     assert cs.checkout("AAAAD") == 195
     assert cs.checkout("AAABB") == 175
     assert cs.checkout("AAAABBB") == 255
-    assert cs.checkout("AAAAAA") == 260
-    assert cs.checkout("AAAAAAA") == 310
+
